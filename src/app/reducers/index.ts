eebc1d05463e851c0fd1dbe9284024f4f7fa329e
Nick Spinosa <nickspinosa1022@gmail.com>
2017-02-26 22:43:47 -0500

    finished config

---
+++ src/app/reducers/index.ts
@@ -0,0 +1,28 @@
+import { ActionReducer } from '@ngrx/store';
+import { compose } from '@ngrx/core/compose';
+import { combineReducers } from '@ngrx/store';
+import { storeFreeze } from 'ngrx-store-freeze';
+import { environment } from '../../environments/environment';
+
+
+//each key should correspond to an imported interface
+export interface State {
+
+};
+
+
+//each key should correspond to an imported reducer
+const reducers = {
+
+};
+
+const developmentReducer: ActionReducer<State> = compose(storeFreeze, combineReducers)(reducers);
+const productionReducer: ActionReducer<State> = combineReducers(reducers);
+
+export function reducer(state: any, action: any) {
+  if (environment.production) {
+    return productionReducer(state, action);
+  } else {
+    return developmentReducer(state, action);
+  }
+}
